64835bff50e9561ef91d9050886379c5d58c981a
Wycliff Byakagaba <spearkickboxing20@gmail.com>
2026-05-10 04:07:39 +0600

    week01/analyze/Program.cs

--- week01/analyze/Program.cs
+++ week01/analyze/Program.cs
@@ -7,7 +7,7 @@ StandardDeviation.Run();
 Console.WriteLine("\n======================\nSearch\n======================");
 Search.Run();
 
-// ===== MY OWN TEST (optional) =====
+// ===== MY OWN TEST (optional) =============
 List<int> data = new List<int> { 1, 2, 3, 4, 5 };
 
 Arrays.RotateListRight(data, 2);
