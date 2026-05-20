611d8b48c6e6e124e12e0485c0234b32142cfbe1
Wycliff Byakagaba <spearkickboxing20@gmail.com>
2026-05-20 20:38:17 +0600

    updated code  week02/code/PriorityQueue.cs

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -5,11 +5,6 @@ public class PriorityQueue
 {
     private List<PriorityItem> _queue = new();
 
-    /// <summary>
-    /// Add a new value to the queue with an associated priority. The
-    /// node is always added to the back of the queue regardless of 
-    /// the priority.
-    /// </summary>
     public void Enqueue(string value, int priority)
     {
         var newNode = new PriorityItem(value, priority);
@@ -23,27 +18,24 @@ public class PriorityQueue
             throw new InvalidOperationException("The queue is empty.");
         }
 
-        // Find the index of the highest priority item
         var highPriorityIndex = 0;
 
         for (int index = 1; index < _queue.Count; index++)
         {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
+            // FIX: only replace when strictly higher priority
+            if (_queue[index].Priority > _queue[highPriorityIndex].Priority)
             {
                 highPriorityIndex = index;
             }
         }
 
-        // Get value to return
         var value = _queue[highPriorityIndex].Value;
 
-        // FIX: actually remove the item from the queue
         _queue.RemoveAt(highPriorityIndex);
 
         return value;
     }
 
-    // DO NOT MODIFY THIS METHOD
     public override string ToString()
     {
         return $"[{string.Join(", ", _queue)}]";
@@ -61,7 +53,6 @@ internal class PriorityItem
         Priority = priority;
     }
 
-    // DO NOT MODIFY THIS METHOD
     public override string ToString()
     {
         return $"{Value} (Pri:{Priority})";
